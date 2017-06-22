00c2b39b453d559e53d8f3a8fae4d4b6aec6cc90
Amit Kulkarni <kulkarniamit@live.com>
2017-06-22 14:29:59 -0700

    Update get_tools.sh

--- get_tools.sh
+++ get_tools.sh
@@ -9,17 +9,15 @@ sudo apt-get install git vim unity-tweak-tool openssh-server
 # Install system cleaner
 sudo apt-get install systemd -f
 wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.7/stacer_1.0.7_amd64.deb
+sleep 5
+sudo apt-get install libappindicator1 -f
 sudo dpkg --install ~/Downloads/stacer_1.0.7_amd64.deb
-
-# Download Google chrome
-firefox "https://www.google.com/chrome/browser/desktop/index.html"
-
-# Install Google chrome
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
 
 # OpenSSH Public-Private key Setup
 ssh-keygen -t rsa -b 8192
 
 # Cleanup
-rm -rf ~/Downloads/google-chrome-stable*
 rm -rf ~/Downloads/stacer_1.0.7_amd64.deb
+
+# Download Google chrome
+firefox "https://www.google.com/chrome/browser/desktop/index.html"
