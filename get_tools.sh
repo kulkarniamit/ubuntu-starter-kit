9dd035a871f7e63e4334e1985662b3ee9d6f7754
Amit Kulkarni <kulkarniamit@live.com>
2017-06-23 15:49:16 -0700

    Update get_tools.sh

--- get_tools.sh
+++ get_tools.sh
@@ -4,14 +4,13 @@
 sudo apt-get update
 
 # Install awesome tools
-sudo apt-get install git vim unity-tweak-tool openssh-server tmux
+sudo apt-get install git vim unity-tweak-tool openssh-server tmux curl
 
 # Install system cleaner
 sudo apt-get install systemd -f
 wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.7/stacer_1.0.7_amd64.deb
 sleep 5
 sudo apt-get install libappindicator1 -f
-sudo dpkg --install ~/Downloads/stacer_1.0.7_amd64.deb
 
 # OpenSSH Public-Private key Setup
 ssh-keygen -t rsa -b 8192
@@ -28,3 +27,7 @@ sudo apt-get autoremove
 
 # Download Google chrome
 firefox "https://www.google.com/chrome/browser/desktop/index.html"
+
+echo "Please install Stacer and Google chrome from ~/Downloads"
+echo "Thank you for using this script"
+echo "Have a great start!"
