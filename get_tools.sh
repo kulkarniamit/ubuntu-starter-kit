8a2180448c71c5777d11ef85eb07cbba2a906b52
Amit Kulkarni <kulkarniamit@live.com>
2017-06-27 17:41:23 -0700

    Update get_tools.sh
    Stacer is trash! 
    Doesn't show any details!
    Let's go with our old friend "bleachbit"

--- get_tools.sh
+++ get_tools.sh
@@ -7,10 +7,7 @@ sudo apt-get update
 sudo apt-get install git vim unity-tweak-tool openssh-server tmux curl
 
 # Install system cleaner
-sudo apt-get install systemd -f
-wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.7/stacer_1.0.7_amd64.deb
-sleep 5
-sudo apt-get install libappindicator1 -f
+sudo apt-get install bleachbit
 
 # OpenSSH Public-Private key Setup
 ssh-keygen -t rsa -b 8192
@@ -22,7 +19,6 @@ sudo apt-get purge aisleriot gnome-sudoku gnome-mahjongg gnome-mines
 sudo apt-get purge thunderbird*
 
 # Cleanup
-rm -rf ~/Downloads/stacer_1.0.7_amd64.deb
 sudo apt-get autoremove
 
 # Download Google chrome
